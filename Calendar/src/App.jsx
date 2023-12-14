f2c10ac89292ecbca0487a132eb3d8e64a4bba84
mnjh01 <119735814+mnjihwan@users.noreply.github.com>
2023-12-14 11:14:07 +0900

    이게무어ㅑ

--- Calendar/src/App.jsx
+++ Calendar/src/App.jsx
@@ -1,35 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
+import moment from 'moment'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [value, onChange] = useState(new Date())
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		<>
+			<Calendar
+				onChange={onChange}
+				value={value}
+				// formatDay={(locale, date) => moment(date).format("DD")}
+				navigationLabel={null}
+				className="mx-auto w-96 text-sm border-b"
+			/>
+		</>
+	)
 }
 
 export default App
